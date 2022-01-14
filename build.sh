aa839de090f3aa59b34154e366fede0cacef7a36
Raffaele <raffio98@gmail.com>
2022-01-15 00:37:25 +0100

    typo fixed in build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 gcc tests/test.c  src/lina.c -o test  -Wall -Wextra -g -Isrc/
-gcc tests/test2.c src/lina.c -o test2 -Wall -Wextra -g -Isrc/
+gcc tests/test_loader.c src/lina.c -o test_loader -Wall -Wextra -g -Isrc/
