f3edd382c9db02c0017585db8e12a3ce367d866f
Raffaele <raffio98@gmail.com>
2022-01-18 10:44:00 +0100

    test unit loading tests from files

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 gcc tests/test.c  src/lina.c -o test  -Wall -Wextra -g -Isrc/
 gcc tests/test_loader.c src/lina.c -o test_loader -Wall -Wextra -g -Isrc/
-
-gcc tests/my_test.c src/lina.c -o my_test -Wall -Wextra -g -Isrc/
